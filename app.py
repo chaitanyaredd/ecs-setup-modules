f4ee279cb6705935e60bed894e77ef42b8188374
developer-1 <developer-1@gmail.com>
2025-05-01 17:26:40 +0530

    Check latest deploy changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from ECS!"
+    return "POC of Deploying simple python application on ECS Completed successfully. Also Infra of ECS is created using Terraform."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
